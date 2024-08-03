0241683e4c97efa2d6c17d4f7ed64d68c6fce8e6
assaf <assafperetz6@gmail.com>
2024-08-03 14:44:54 +0300

    Add basic img rendering

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,1 +1,39 @@
 'use strict'
+const gElCanvas = document.querySelector('.meme-editor canvas')
+const gCtx = gElCanvas.getContext('2d')
+
+function onInit() {
+    resizeCanvas()
+    renderMeme()
+}
+
+function resizeCanvas() {
+    const elCanvasContainer = document.querySelector('.canvas-container')
+
+    gElCanvas.width = elCanvasContainer.clientWidth
+    gElCanvas.height = elCanvasContainer.clientHeight
+}
+
+function resizeCanvasToImg(img) {
+    const elCanvasContainer = document.querySelector('.canvas-container')
+
+    elCanvasContainer.style.width = img.naturalWidth
+    elCanvasContainer.style.height = img.naturalHeight
+
+    resizeCanvas()
+}
+
+function renderMeme() {
+    const meme = getMeme()
+    const img = getMemeImg()
+
+    drawImg(img.url)
+}
+
+function drawImg(imgUrl) {
+    const img = new Image()
+    img.src = imgUrl
+
+    resizeCanvasToImg(img)
+    img.onload = () => gCtx.drawImage(img, 0, 0)
+}
--- js/meme.service.js
+++ js/meme.service.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const gImgs = [{ id: 1, url: 'meme-imgs/meme-imgs (square)/1.jpg', keywords: ['funny', 'cat'] }]
+const gMeme = {
+	selectedImgId: 1,
+	selectedLineIdx: 0,
+	lines: [
+		{
+			txt: 'I sometimes eat Falafel',
+			size: 20,
+			color: 'red',
+		},
+	],
+}
+
+var gKeywordSearchCountMap = { funny: 12, cat: 16, baby: 2 }
+
+function getMeme() {
+    return gMeme
+}
+
+function getMemeImg() {
+    return gImgs.find(({ id }) => id === gMeme.selectedImgId)
+}
