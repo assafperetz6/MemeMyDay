addf0593c2c33941580286bbbbf0f391bfae4944
assaf <assafperetz6@gmail.com>
2024-08-08 19:31:44 +0300

    Add filter for gallery

--- js/data.service.js
+++ js/data.service.js
@@ -1,15 +1,15 @@
 'use strict'
 
 const gImgs = [
-    { id: 1, url: 'meme-imgs/meme-imgs (square)/1.jpg', keywords: ['funny', 'cat'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/2.jpg', keywords: ['funny', 'cat'] },
-    { id: 3, url: 'meme-imgs/meme-imgs (various aspect ratios)/003.jpg', keywords: ['funny', 'cat'] },
-    { id: 4, url: 'meme-imgs/meme-imgs (square)/4.jpg', keywords: ['funny', 'cat'] },
-    { id: 5, url: 'meme-imgs/meme-imgs (square)/5.jpg', keywords: ['funny', 'cat'] },
-    { id: 6, url: 'meme-imgs/meme-imgs (square)/6.jpg', keywords: ['funny', 'cat'] },
-    { id: 7, url: 'meme-imgs/meme-imgs (square)/7.jpg', keywords: ['funny', 'cat'] },
-    { id: 8, url: 'meme-imgs/meme-imgs (square)/8.jpg', keywords: ['funny', 'cat'] },
-    { id: 9, url: 'meme-imgs/meme-imgs (square)/9.jpg', keywords: ['funny', 'cat'] },
+    { id: 1, url: 'meme-imgs/meme-imgs (square)/1.jpg', keywords: ['politics', 'trump'] },
+    { id: 2, url: 'meme-imgs/meme-imgs (square)/2.jpg', keywords: ['dog', 'animals'] },
+    { id: 3, url: 'meme-imgs/meme-imgs (various aspect ratios)/drevil.jpg', keywords: ['funny', 'pop culture'] },
+    { id: 4, url: 'meme-imgs/meme-imgs (square)/4.jpg', keywords: ['cute', 'cat'] },
+    { id: 5, url: 'meme-imgs/meme-imgs (square)/5.jpg', keywords: ['baby', 'success'] },
+    { id: 6, url: 'meme-imgs/meme-imgs (square)/6.jpg', keywords: ['funny', 'history'] },
+    { id: 7, url: 'meme-imgs/meme-imgs (square)/7.jpg', keywords: ['funny', 'baby', 'surprising'] },
+    { id: 8, url: 'meme-imgs/meme-imgs (square)/8.jpg', keywords: ['pop culture', 'quirky'] },
+    { id: 9, url: 'meme-imgs/meme-imgs (square)/9.jpg', keywords: ['funny', 'baby'] },
 ]
 
 const gSentences = [
--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -1,78 +1,102 @@
 'use strict'
 
-function renderGallery() {
-    const imgs = getImgs()
-    const htmlStrs = imgs.map(({ url, id }) => `<img src="${url}" alt="" onclick="onImgSelect(${id})"></img>`).join('')
-    const elGallery = document.querySelector('.gallery')
+function renderGallery(imgs = getImgs()) {
+	const htmlStrs = imgs
+		.map(
+			({ url, id }) =>
+				`<img src="${url}" alt="" onclick="onImgSelect(${id})"></img>`
+		)
+		.join('')
+	const elGallery = document.querySelector('.gallery')
 
-    elGallery.innerHTML = htmlStrs
+	elGallery.innerHTML = htmlStrs
 }
 
 function onShowSavedMemes() {
-    renderSavedMemes()
-    showGallery(true)
+	renderSavedMemes()
+	showGallery(true)
 }
 
 function renderSavedMemes() {
-    const savedMemes = loadSavedMemes() || []
-    const htmlStrs = savedMemes.map(({ memeThumbnail }, idx) => {
-        return `<div class="saved-meme-container">
+	const savedMemes = loadSavedMemes() || []
+	const htmlStrs = savedMemes
+		.map(({ memeThumbnail }, idx) => {
+			return `<div class="saved-meme-container">
                     <span class="btn delete-meme flex justify-center align-center" onclick="onDeleteMeme(${idx})">X</span>
                     <img src="${memeThumbnail}" alt="" onclick="onRenderSavedMeme(${idx})"></img>
                 </div>`
-        }).join('')
-    const elGallery = document.querySelector('.gallery')
+		})
+		.join('')
+	const elGallery = document.querySelector('.gallery')
 
-    elGallery.innerHTML = htmlStrs || '<h2>There are currently no saved memes</h2>'
+	elGallery.innerHTML =
+		htmlStrs || '<h2>There are currently no saved memes</h2>'
 
-    if (elGallery.classList.contains('hidden')) showGallery()
+	if (elGallery.classList.contains('hidden')) showGallery()
 }
 
 function onRenderSavedMeme(idx) {
-    const savedMemes = loadSavedMemes()
+	const savedMemes = loadSavedMemes()
 
-    loadMemeToEdit(savedMemes[idx].gMeme)
-    renderMeme()
+	loadMemeToEdit(savedMemes[idx].gMeme)
+	renderMeme()
 }
 
 function onImgSelect(imgId) {
-    createNewMeme()
-    setImg(imgId)
-    renderMeme()
+	createNewMeme()
+	setImg(imgId)
+	renderMeme()
+}
+
+function onFilterGallery(filterTxt) {
+    const normalizedTxt = filterTxt.toLowerCase()
+	const imgs = getImgs()
+	const filteredImgs = imgs.filter(({ keywords }) =>
+		keywords.find((keyword) => keyword.includes(normalizedTxt))
+	)
+
+	renderGallery(filteredImgs)
+}
+
+function onClearSearchBar() {
+	const elSearchBar = document.getElementById('search-input')
+	elSearchBar.value = ''
+
+    renderGallery()
 }
 
 function showGallery(isSavedMemes) {
-    const elGallery = document.querySelector('.gallery-container')
-    const elEditor = document.querySelector('.meme-editor')
-    
-    if (!isSavedMemes) renderGallery()
-    
-    elGallery.classList.remove('hidden')
-    elEditor.classList.add('hidden')
+	const elGallery = document.querySelector('.gallery-container')
+	const elEditor = document.querySelector('.meme-editor')
+
+	if (!isSavedMemes) renderGallery()
+
+	elGallery.classList.remove('hidden')
+	elEditor.classList.add('hidden')
 }
 
 function showMemeEditor() {
-    const elGallery = document.querySelector('.gallery-container')
-    const elEditor = document.querySelector('.meme-editor')
+	const elGallery = document.querySelector('.gallery-container')
+	const elEditor = document.querySelector('.meme-editor')
 
-    elEditor.classList.remove('hidden')
-    elGallery.classList.add('hidden')
+	elEditor.classList.remove('hidden')
+	elGallery.classList.add('hidden')
 }
 
 function onGenerateRandMeme() {
-    const { id } = getRandImg()
+	const { id } = getRandImg()
 
-    setImg(id)
-    createRandMeme(id)
-    renderMeme()
-    showMemeEditor()
+	setImg(id)
+	createRandMeme(id)
+	renderMeme()
+	showMemeEditor()
 }
 
 function onDeleteMeme(memeIdx) {
-    let savedMemes = loadSavedMemes()
-    
-    savedMemes.splice(memeIdx, 1)
+	let savedMemes = loadSavedMemes()
 
-    saveToStorage('savedMemes', savedMemes)
-    renderSavedMemes()
+	savedMemes.splice(memeIdx, 1)
+
+	saveToStorage('savedMemes', savedMemes)
+	renderSavedMemes()
 }
